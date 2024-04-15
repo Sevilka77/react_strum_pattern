80241b9183a3f66347765d812079679a8d123d1c
Sevilka77 <sevil-perm@mail.ru>
2024-04-15 16:57:13 +0500

    fix drawer

--- src/components/PatternLIst.jsx
+++ src/components/PatternLIst.jsx
@@ -1,20 +1,36 @@
 import { patterns } from "../patterns"
+import { useState } from "react";
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
+import { Box, Button, Drawer } from "@mui/material";
 
 export default function PatternList({ onPatternChanged }) {
-  return (
-    <List>
-      {patterns.map((pattern, index) => (
-        <ListItem disablePadding key={index}  >
-          <ListItemButton>
-            <ListItemText key={pattern.title} onClick={() => onPatternChanged(pattern)} primary={pattern.title} />
-          </ListItemButton>
-        </ListItem>
+  const [open, setOpen] = useState(false);
 
-      ))}
-    </List>
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        {patterns.map((pattern, index) => (
+          <ListItem disablePadding key={index}  >
+            <ListItemButton>
+              <ListItemText key={pattern.title} onClick={() => onPatternChanged(pattern)} primary={pattern.title} />
+            </ListItemButton>
+          </ListItem>))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <div>
+      <Button onClick={toggleDrawer(true)}>Выбрать бой</Button>
+      <Drawer open={open} onClose={toggleDrawer(false)}>
+        {DrawerList}
+      </Drawer>
+    </div>
   )
 }
