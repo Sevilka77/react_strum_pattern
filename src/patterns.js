3b2cc989c32131ec49ac1bc96371e0bdf80aafc2
Sevilka77 <sevil-perm@mail.ru>
2024-04-15 17:11:19 +0500

    22

--- src/patterns.js
+++ src/patterns.js
@@ -24,63 +24,63 @@ pattern:"101111"
 pattern:"100111"
 },
 {
-  title: 'Галоп 4ка',
-pattern:"10101011"
+  title: 'Hjr',
+pattern:"1010Ф011"
 },
 {
-  title: 'Галоп 4ка',
-pattern:"10110111"
+  title: '6ка обычная',
+  pattern:"10110111"
 },
 {
-  title: 'Галоп 4ка',
+  title: '6 с заглушкой (6х)',
 pattern:"10x101x1"
 },
 {
-  title: 'Галоп 4ка',
-pattern:"10110010"
+  title: '6ка кантри',
+pattern:"10A100A0"
 },
 {
-  title: 'Галоп 4ка',
-pattern:"10111111"
+  title: '6 Кантри1',
+pattern:"10A111A1"
 },
 {
-  title: 'Галоп 4ка',
+  title: '6 Кантри c заглуш',
 pattern:"10x111x1"
 },
 {
-  title: 'Галоп 4ка',
+  title: '8Упрощенная',
 pattern:"10011111"
 },
 {
-  title: 'Галоп 4ка',
+  title: '8я малая',
 pattern:"10001011"
 },
 {
-  title: '↓   ↓X↑ ',
+  title: 'Варвара ',
 pattern:"1001x10"
 },
 {
-  title: 'Галоп 4а',
+  title: 'Рэгги',
 pattern:"1011x01x"
 },
 {
-  title: 'Галоп 4ка',
+  title: '8ка стандартная',
 pattern:"1000101101101010"
 },
 {
-  title: 'Галоп 4ка',
+  title: '8+ (быстрая)',
 pattern:"A000A0111110A011"
 },
 {
-  title: 'Галоп 4ка',
+  title: '8 перк (перкуссионная)',
 pattern:"A111A11A1A11A111x"
 },
 {
-  title: 'Галоп 4ка',
+  title: '8ка слабые доли',
 pattern:"A00A11A00A11A011"
 },
 {
-  title: 'Галоп 4ка',
+  title: '8ка с заглушками',
 pattern:"1001x1100100x111"
 },
 ]
