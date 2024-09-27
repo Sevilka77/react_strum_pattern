404ebb036cc052e0c44abb3108ad921917c71478
Sevilka77 <sevil-perm@mail.ru>
2024-09-27 17:00:10 +0500

    11

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,21 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ConfigProvider } from "./ConfigProvider.jsx";
+const YandexMetrika =
+  import.meta.env.MODE === "production"
+    ? React.lazy(() => import("./components/YandexMetrika"))
+    : null;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <ConfigProvider>
         <App />
+        {YandexMetrika && (
+          <React.Suspense fallback={<div>Loading...</div>}>
+            <YandexMetrika />
+          </React.Suspense>
+        )}
       </ConfigProvider>
     </Router>
   </React.StrictMode>,
