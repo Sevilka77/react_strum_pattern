bc77dc27969b95b96b4b3e0d161be2502747921b
Sevilka77 <sevil-perm@mail.ru>
2025-11-18 16:09:56 +0500

    11

--- src/components/YandexMetrika.jsx
+++ src/components/YandexMetrika.jsx
@@ -1,46 +1,25 @@
-import { useEffect } from "react";
+const METRIKA_ID = import.meta.env.VITE_YANDEX_METRIKA_ID;
 
-const YandexMetrika = () => {
-  useEffect(() => {
-    // Функция для загрузки скрипта Яндекс.Метрики
-    const loadMetrika = () => {
-      if (typeof window !== "undefined" && !window.ym) {
-        (function (m, e, t, r, i, k, a) {
-          m[i] =
-            m[i] ||
-            function () {
-              (m[i].a = m[i].a || []).push(arguments);
-            };
-          m[i].l = 1 * new Date();
-          (k = e.createElement(t)),
-            (a = e.getElementsByTagName(t)[0]),
-            (k.async = 1),
-            (k.src = r),
-            a.parentNode.insertBefore(k, a);
-        })(
-          window,
-          document,
-          "script",
-          "https://mc.yandex.ru/metrika/tag.js",
-          "ym"
-        );
-      }
-    };
+useEffect(() => {
+  if (!METRIKA_ID || typeof window === "undefined") return;
 
-    // Загрузка скрипта и инициализация метрики
-    loadMetrika();
-    const METRIKA_ID = import.meta.env.VITE_YANDEX_METRIKA_ID;
-    // После загрузки скрипта инициализируем Яндекс.Метрику
-    if (typeof window !== "undefined") {
-      window.ym(METRIKA_ID, "init", {
-        clickmap: true,
-        trackLinks: true,
-        accurateTrackBounce: true,
-      });
-    }
-  }, []);
+  (function (m, e, t, r, i, k, a) {
+    m[i] =
+      m[i] ||
+      function () {
+        (m[i].a = m[i].a || []).push(arguments);
+      };
+    m[i].l = 1 * new Date();
+    k = e.createElement(t);
+    a = e.getElementsByTagName(t)[0];
+    k.async = 1;
+    k.src = r;
+    a.parentNode.insertBefore(k, a);
+  })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-  return null; // Компонент ничего не рендерит
-};
-
-export default YandexMetrika;
+  window.ym(METRIKA_ID, "init", {
+    clickmap: true,
+    trackLinks: true,
+    accurateTrackBounce: true,
+  });
+}, []);
